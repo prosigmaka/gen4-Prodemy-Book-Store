78566f5ae25d0ee82167779099fb79ef53293425
Nekkyoo <nekkyoo.meishi.s@gmail.com>
2021-04-27 15:41:01 +0700

    update fe login dan registration

--- src/main/java/com/example/onlinebookstore/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/example/onlinebookstore/configuration/WebSecurityConfiguration.java
@@ -43,7 +43,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 				 "/registration**",
 	                "/js/**",
 	                "/css/**",
-	                "/img/**", "/home*").permitAll()
+	                "/img/**", "/aboutus*", "/home*", "/home-company*", "/product*").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
