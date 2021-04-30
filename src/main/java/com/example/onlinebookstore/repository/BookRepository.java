1108d0edaaffb4dd9eca7681cdccb3729d9c8bc4
himawanadityas <adityashimawan@yahoo.com>
2021-04-30 07:13:01 +0700

    create category pages api and repository

--- src/main/java/com/example/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.entity.Book;
 import io.swagger.models.auth.In;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,6 +23,5 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     @Query(value = "select harga_buku from book where id = ?1", nativeQuery = true)
     Long getHargaById(Integer id);
 
-    List<Book> findAllByCategory(String cat, Pageable pageable);
 
 }
